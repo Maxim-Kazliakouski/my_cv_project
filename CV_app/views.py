6cb37c1fc4dc3a489240aef6f5ecbaad1d216bb3
Maxim Kazliakouski <maxim.kazliakouski@gmail.com>
2021-02-15 00:29:27 +0300

    adding info in views.py file

--- CV_app/views.py
+++ CV_app/views.py
@@ -2,6 +2,37 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def main_page(request):
-    return render (request, 'CV_app/CV_main_page.html')
-    
+def main_layout(request):
+    return render (request, 'CV_app/main_layout.html')
+
+
+def about_me(request):
+    return render (request, 'CV_app/about_me.html')
+
+
+def person_page(request):
+    return render (request, 'CV_app/personal_info.html')
+
+def work(request):
+    return render (request, 'CV_app/work_expirience.html')
+
+def edu(request):
+    return render (request, 'CV_app/education.html')
+
+def lang(request):
+    return render (request, 'CV_app/languages.html')
+
+def skills(request):
+    return render (request, 'CV_app/skills.html')
+
+def cert(request):
+    return render (request, 'CV_app/certificates.html')
+
+def hob(request):
+    return render (request, 'CV_app/hobbies.html')
+
+def per_inf_skype(request):
+    return render (request, 'CV_app/personal_info_skype.html')    
+
+def per_inf_phone(request):
+    return render (request, 'CV_app/personal_info_phone.html')
