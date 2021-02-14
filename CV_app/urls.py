bffd9bee8c37bb3e9cb7697c57f81fa3ea878178
Maxim Kazliakouski <maxim.kazliakouski@gmail.com>
2021-02-15 00:23:33 +0300

    pushing new app

--- CV_app/urls.py
+++ CV_app/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path ('', views.main_page),
-]
+    path('', views.about_me, name='info_about_me'),
+    path('about_me/', views.about_me, name='info_about_me'),
+    path('personal_info/', views.person_page, name='per_info'),
+    path('work_expirience/', views.work, name='work'),
+    path('education/', views.edu, name='edu'),
+    path('languages/', views.lang, name='lang'),
+    path('skills/', views.skills, name='skills'),
+    path('certificates/', views.cert, name='cert'),
+    path('hobbies/', views.hob, name='hobbies'),
+    path('personal_info_skype/', views.per_inf_skype, name='per_skype'),
+    path('personal_info_phone', views.per_inf_phone, name='per_phone'),
+    ]
